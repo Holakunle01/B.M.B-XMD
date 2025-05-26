910578aca0e30abb0199c4d44b4b3504623a7b6c
Holakunle01 <162513493+Holakunle01@users.noreply.github.com>
2025-05-26 21:16:50 +0100

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "BMB-XMD~rEhx0S7Y#w6m5f6cq2qY4gZX5D1Z7wsXQ3tOVWVgev-cZkN7owYk",
 // add your Session Id
 AUTO_BIO: process.env.AUTO_BIO || "false", 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
